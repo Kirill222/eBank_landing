30e2e976e07bb3481352592661d271f712e1c1ba
Zzz <zkmd2012@inbox.ru>
2022-01-27 11:15:09 +0100

    added HashRoute to solve the issue with blank page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter base='/'>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-
+)
