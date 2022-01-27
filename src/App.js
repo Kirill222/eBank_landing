ae56eb9c40a1d422f4c31ccbcfa176a47388a53d
Zzz <zkmd2012@inbox.ru>
2022-01-27 11:28:32 +0100

    wfww

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css'
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages'
 import SigninPage from './pages/signin'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/signin' component={SigninPage}/>
-      </Switch>
-      
-    </Router>
-  );
+    // <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/signin' component={SigninPage} />
+    </Switch>
+
+    // </Router>
+  )
 }
 
-export default App;
+export default App
